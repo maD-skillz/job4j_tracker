4c992d1420e274bc8e2a6eec4db7776cafe99dfe
maD-skillz <warneverchang3s@gmail.com>
2021-01-05 11:05:43 +0300

    1.7. Состояние объекта [#331662]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,6 +8,7 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
+        this.load = another.load + this.load;
+        this.load = 0;
     }
 }
