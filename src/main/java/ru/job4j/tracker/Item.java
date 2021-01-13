4c467bc7afb75eded5a6b2c17ebb85803cc9c2d3
maD-skillz <warneverchang3s@gmail.com>
2021-01-13 12:12:27 +0300

    2. Перегрузка конструктора [#331685]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,22 @@
 package ru.job4j.tracker;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
+
     private int id;
+
     private String name;
 
-    public int getId() {
-        return id;
+    public Item First() {
+        return First();
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public Item Second(int id) {
+        return Second(id);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public Item Third(int id, String name) {
+        return Third(id, name);
     }
 }
