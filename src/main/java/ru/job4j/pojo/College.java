5e3f253939dfb6f279733cde583051a546b7a5d2
maD-skillz <warneverchang3s@gmail.com>
2021-01-28 14:30:40 +0300

    2. Модель данных. [#331693]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,16 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Peter");
+        student.setGroup("1");
+        student.setCreated(new Date());
+
+        System.out.println("Студент " + student.getName() + " учится в руппе номер " + student.getGroup() + " c " + student.getCreated());
+
+    }
+}
