0e30b4be8da5e999fe6bad0766b04c5b255f7909
maD-skillz <warneverchang3s@gmail.com>
2021-01-28 11:09:19 +0300

    5. Аннотация @Override [#331687]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,7 +2,8 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
-    public static void main(String[] args) {
+    @Override
+    public void main(String[] args) {
         TextReport JSONReport = new TextReport();
         String text = JSONReport.generate("name", "body");
         System.out.println(text);
