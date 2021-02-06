a142646beb9124597ff4fa4a5a6d3f57e0f897d6
maD-skillz <warneverchang3s@gmail.com>
2021-02-06 14:10:40 +0300

    7. Метод удаления заявки Tracker.delete [#331698]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -62,7 +62,8 @@ public class Tracker {
             System.arraycopy(items, index + 1, items, index, size - index);
             items[size - 1] = null;
             size--;
+            return true;
         }
-        return true;
+       return false;
     }
 }
