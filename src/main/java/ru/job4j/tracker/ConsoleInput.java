974aac73abd5e6f1facc9caec403094691a91b09
maD-skillz <warneverchang3s@gmail.com>
2021-02-23 18:24:35 +0300

    9.1 Зависимость от System.out [#331619]

--- src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 public class ConsoleInput implements Input {
-    final Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     @Override
     public String askStr(String question) {
