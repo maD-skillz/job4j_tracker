3ffa9f7b132f43f7b8f3f026ada7a7812124bca5
maD-skillz <warneverchang3s@gmail.com>
2021-02-27 13:20:07 +0300

    0.3. Тестирование исключений с junit [#331642]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -1,10 +1,6 @@
 package ru.job4j.ex;
 
 public class Fact {
-    public static void main(String[] args) {
-
-    }
-
     public int calc(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("'n' should be less then zero.");
