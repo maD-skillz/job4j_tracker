fcc4d71739d8c555d09f39e5941644ac39e7e9bd
maD-skillz <warneverchang3s@gmail.com>
2021-03-05 15:17:29 +0300

    0.4. Пользовательские исключения. [#331643]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,21 +3,25 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length; i++ ) {
-            if (rsl == -1) {
-                throw new ElementNotFoundException("Not Found");
-            } else if (value[i] == key) {
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] == key) {
                 return i;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Not Found");
         }
         return rsl;
     }
 
-    public static void main(String[] args) {
-        try {
-          FindEl.indexOf();
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
+
+        public static void main (String[]args){
+        String[] arr = new String[] {"a", "b", "c"};
+            try {
+                FindEl.indexOf(arr, "z");
+            } catch (ElementNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
-}
+
