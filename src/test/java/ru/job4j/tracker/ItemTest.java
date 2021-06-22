da5a3c8b662ae2cf43b9214ff6e6a8fa5b51e697
maD-skillz <warneverchang3s@gmail.com>
2021-06-22 13:34:23 +0300

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,22 +15,52 @@ public class ItemTest {
     public void whenSortToHigh() {
         Item cat = new Item();
         Item dog = new Item();
-        cat.setId(1);
-        cat.setName("Cat");
+        Item fish = new Item();
+
+        fish.setId(3);
+        fish.setName("Fish");
         dog.setId(2);
         dog.setName("Dog");
-        assertThat(cat.compareTo(dog) , is(-1));
+        cat.setId(1);
+        cat.setName("Cat");
+
+        List<Item> itemList = new ArrayList<>();
+        itemList.add(fish);
+        itemList.add(dog);
+        itemList.add(cat);
+        Collections.sort(itemList, new SortAscByName());
+
+        List<Item> expectedList = new ArrayList<>();
+        expectedList.add(cat);
+        expectedList.add(dog);
+        expectedList.add(fish);
+        assertThat(itemList , is(expectedList));
     }
 
     @Test
     public void whenSortToLow() {
         Item cat = new Item();
         Item dog = new Item();
+        Item fish = new Item();
+
         cat.setId(1);
         cat.setName("Cat");
+        fish.setId(3);
+        fish.setName("Fish");
         dog.setId(2);
         dog.setName("Dog");
-        assertThat(dog.compareTo(cat), is(1));
+
+        List<Item> itemList = new ArrayList<>();
+        itemList.add(fish);
+        itemList.add(dog);
+        itemList.add(cat);
+        Collections.sort(itemList, new SortDescByName());
+
+        List<Item> expectedList = new ArrayList<>();
+        expectedList.add(fish);
+        expectedList.add(dog);
+        expectedList.add(cat);
+        assertThat(itemList , is(expectedList));
     }
 
 }
