38ed69f9eafc9e00b05e0ff3dd38e3054c7b6df7
maD-skillz <warneverchang3s@gmail.com>
2021-07-09 18:16:20 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,14 +5,10 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        for (int i = 0; i < Math.min(o1.length(), o2.length()); i++) {
-            char ch1 = o1.charAt(i);
-            char ch2 = o2.charAt(i);
-            if (ch1 != ch2) {
-                return Character.compare(ch1, ch2);
-            }
-        }
-        return Integer.compare(o1.length(), o2.length());
+        String[] left = o1.split("/");
+        String[] right = o2.split("/");
+        int rsl = right[0].compareTo(left[0]);
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
 
