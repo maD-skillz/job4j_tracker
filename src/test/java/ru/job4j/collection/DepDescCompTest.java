7a5c81c37f9eabafb09c4f39869519dedd698f61
maD-skillz <warneverchang3s@gmail.com>
2021-07-10 17:01:55 +0300

    2. Отсортировать департаменты [#34352]

--- src/test/java/ru/job4j/collection/DepDescCompTest.java
+++ src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -28,15 +28,6 @@ public class DepDescCompTest {
     }
 
     @Test
-    public void whenUpDepartmentEqual() {
-        int rsl = new DepDescComp().compare(
-                "K2/SK1/SSK2",
-                "K2/SK1/SSK2"
-        );
-        assertThat(rsl, is(0));
-    }
-
-    @Test
     public void whenAscSorted() {
         List<String> input = Arrays.asList("k2", "k1", "k1/sk1", "k1/sk1/ssk1", "k2/sk1");
         List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1", "k2", "k2/sk1");
