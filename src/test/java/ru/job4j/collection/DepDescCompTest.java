1a0e2b564d4f140851c6e9dd29d83ec35a554a8b
maD-skillz <warneverchang3s@gmail.com>
2021-07-10 16:56:14 +0300

    2. Отсортировать департаменты [#34352]

--- src/test/java/ru/job4j/collection/DepDescCompTest.java
+++ src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -1,10 +1,10 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-
-import static org.hamcrest.Matchers.lessThan;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.greaterThan;
 
 public class DepDescCompTest {
 
@@ -25,5 +25,36 @@ public class DepDescCompTest {
                 "K2/SK1"
         );
         assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenUpDepartmentEqual() {
+        int rsl = new DepDescComp().compare(
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK2"
+        );
+        assertThat(rsl, is(0));
+    }
+
+    @Test
+    public void whenAscSorted() {
+        List<String> input = Arrays.asList("k2", "k1", "k1/sk1", "k1/sk1/ssk1", "k2/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1", "k2", "k2/sk1");
+        Departments.sortAsc(input);
+        assertThat(input, is(expect));
+    }
+    @Test
+    public void whenDescSorted1() {
+        List<String> input = Arrays.asList("k2", "k1", "k1/sk1", "k1/sk1/ssk1", "k2/sk1");
+        List<String> expect = Arrays.asList("k2", "k2/sk1", "k1", "k1/sk1", "k1/sk1/ssk1");
+        Departments.sortDesc(input);
+        assertThat(input, is(expect));
+    }
+    @Test
+    public void whenDescSorted2() {
+        List<String> input = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1", "k2", "k2/sk1");
+        List<String> expect = Arrays.asList("k2", "k2/sk1", "k1", "k1/sk1", "k1/sk1/ssk1");
+        Departments.sortDesc(input);
+        assertThat(input, is(expect));
     }
 }
