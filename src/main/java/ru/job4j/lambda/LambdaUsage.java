f69be26b9e6d9be038e4bb57f3b9316c5a40c9b5
maD-skillz <warneverchang3s@gmail.com>
2021-07-11 18:02:46 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,12 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<Attachment> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.getSize() + " : " + left.getSize());
+            return Integer.compare(right.getSize(), left.getSize());
+        };
+    }
+}
