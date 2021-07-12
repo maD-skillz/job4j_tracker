37f8cc7f55f4bf30378bef86f484ad048fa5d164
maD-skillz <warneverchang3s@gmail.com>
2021-07-12 12:15:43 +0300

    5. Подсчет функции в диапазоне. [#24251]

--- src/test/java/ru/job4j/lambda/FunctionCalcTest.java
+++ src/test/java/ru/job4j/lambda/FunctionCalcTest.java
@@ -7,23 +7,27 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FunctionCalcTest {
+
     @Test
     public void whenLinearFunctionThenLinearResults() {
         List<Double> result = FunctionCalc.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D, 17D);
         assertThat(result, is(expected));
     }
+
     @Test
     public void whenExponentialFunctionThenExponentialResults() {
         List<Double> result = FunctionCalc.diapason(0, 10, x -> x * x);
-        List<Double> expected = Arrays.asList(0D, 1D, 4D, 9D, 16D, 25D, 36D, 49D, 64D, 81D, 100D);
+        List<Double> expected = Arrays.asList(
+                0D, 1D, 4D, 9D, 16D, 25D, 36D, 49D, 64D, 81D, 100D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenQuadraticFunctionThenQuadraticResults() {
         List<Double> result = FunctionCalc.diapason(0, 10, x -> Math.pow(2D, x));
-        List<Double> expected = Arrays.asList(1D, 2D, 4D, 8D, 16D, 32D, 64D, 128D, 256D, 512D, 1024D);
+        List<Double> expected = Arrays.asList(
+                1D, 2D, 4D, 8D, 16D, 32D, 64D, 128D, 256D, 512D, 1024D);
         assertThat(result, is(expected));
     }
 }
