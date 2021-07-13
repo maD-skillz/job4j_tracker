eb0939a78809b120bc40184e9a48497db00bddb8
maD-skillz <warneverchang3s@gmail.com>
2021-07-13 18:40:55 +0300

    0. Stream API [#58166]

--- src/main/java/ru/job4j/list/ListUsage.java
+++ src/main/java/ru/job4j/list/ListUsage.java
@@ -7,15 +7,18 @@ import java.util.stream.Collectors;
 public class ListUsage {
     public static void main(String[] args) {
         ArrayList<Integer> arrList = new ArrayList<>();
+        arrList.add(-2);
+        arrList.add(-1);
+        arrList.add(0);
         arrList.add(1);
         arrList.add(2);
         arrList.add(3);
         arrList.add(4);
         arrList.add(5);
         arrList.add(6);
-        List<Integer> even = arrList.stream().filter((x) ->
-                (x % 2 == 0)).collect(Collectors.toList());
-        for (int i : even) {
+        List<Integer> positiveNumbers = arrList.stream().filter((x) ->
+                (x > 0)).collect(Collectors.toList());
+        for (int i : positiveNumbers) {
             System.out.println(i);
         }
     }
