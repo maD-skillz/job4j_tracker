64bfaaf0ef542076c5710174332cffc4b8c1b60a
maD-skillz <warneverchang3s@gmail.com>
2021-07-18 12:53:20 +0300

    9. Шаблоны проектирования в Stream API [#310012]

---
+++ src/main/java/ru/job4j/stream/BuilderCar.java
@@ -0,0 +1,77 @@
+package ru.job4j.stream;
+
+public class BuilderCar {
+    private String name;
+
+    private String model;
+
+    private int year;
+
+    private int maxSpeed;
+
+    private int range;
+
+    private boolean insurance;
+
+    static class Builder {
+        private String name;
+        private String model;
+        private int year;
+        private int maxSpeed;
+        private int range;
+        private boolean insurance;
+
+        Builder builderName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        Builder builderModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder builderYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        Builder builderMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        Builder builderRange(int range) {
+            this.range = range;
+            return this;
+        }
+
+        Builder builderInsurance(boolean insurance) {
+            this.insurance = insurance;
+            return this;
+        }
+
+        BuilderCar build() {
+            BuilderCar builderCar = new BuilderCar();
+            builderCar.name = name;
+            builderCar.model = model;
+            builderCar.year = year;
+            builderCar.maxSpeed = maxSpeed;
+            builderCar.range = range;
+            builderCar.insurance = insurance;
+            return builderCar;
+        }
+    }
+
+    public static void main(String[] args) {
+        BuilderCar builderCar = new Builder().builderName("Chevrolet")
+                .builderModel("Camaro")
+                .builderYear(1999)
+                .builderMaxSpeed(250)
+                .builderRange(500)
+                .builderInsurance(true)
+                .build();
+        System.out.println(builderCar);
+    }
+}
+
